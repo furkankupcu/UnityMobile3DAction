7ba7479f57009602bf5dee02f715dea35d169332
Muhammet Furkan Küpçü <51542849+furkankupcu@users.noreply.github.com>
2024-01-05 22:18:20 +0300

    -Added
    Line renderer

--- Assets/Scripts/Player/GunRaycast.cs
+++ Assets/Scripts/Player/GunRaycast.cs
@@ -1,12 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GunRaycast : MonoBehaviour
 {
+    [SerializeField] LineRenderer _lineRenderer;
+
     public RaycastHit SendRaycast(Transform spawnPoint, float range, int gunMask)
     {
         Ray ray = new Ray(spawnPoint.position, spawnPoint.forward);
         return Physics.Raycast(ray, out RaycastHit hit, range, gunMask) ? hit : default;
     }
+
+    public void EnableLineRenderer() => _lineRenderer.enabled = true;
+
+    public void DisableLineRenderer() => _lineRenderer.enabled = false;
+
 }
